a00a28f2b30cb0d8ff97d2198da0612dd9131354
Arhan Popli <arhanpopli123@outlook.com>
2026-09-24 13:51:29 +0530

    Update whileloop.c to accept user input

--- whileloop.c
+++ whileloop.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
     int main() {
-        int i=10;
+        int i=0;
+        printf("Enter any number = ");
+        scanf("%d",&i);
+        
         while(i>=1)
         {
             printf("%d\n",i);
